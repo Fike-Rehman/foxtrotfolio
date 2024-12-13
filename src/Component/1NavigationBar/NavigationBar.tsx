a9b124ba4aeea35f1fec4d1942032018a7cb8dc9
Fike Rehman <fike.rehman@hotmail.com>
2024-12-13 16:33:26 -0600

    added Nav bar component. Links need more styling adjustments

---
+++ src/Component/1NavigationBar/NavigationBar.tsx
@@ -0,0 +1,23 @@
+import { Nav, Navbar } from 'react-bootstrap';
+import './NavigationBar.css';
+
+interface NavigationBarProps {
+  // Add any props you want to pass to the component
+}
+
+const NavigationBar: React.FC<NavigationBarProps> = () => {
+  return (
+    <Navbar bg="dark" expand="lg" className="navbar">
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="nav-bar"> 
+        <Nav.Link href="#home" className="mx-auto nav-home">Home</Nav.Link>
+          <Nav.Link href="#about" className='nav-link'>About</Nav.Link>
+          <Nav.Link href="#projects" className='nav-link'>Projects</Nav.Link>
+          <Nav.Link href="#resume" className='nav-link'>Resume</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default NavigationBar;
