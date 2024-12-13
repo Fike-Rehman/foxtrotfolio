34367ac04574ca375ac5976e64e601a71af5ec38
frehmanOnQC <fike.rehman@gmail.com>
2024-12-13 11:06:37 -0600

    updated meta tags. started adding the nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Nav, Navbar } from 'react-bootstrap'
 
 function App() {
-  
+
   return (
     <>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">Home</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">About</Nav.Link>
+            <Nav.Link href="#link">Projects</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
