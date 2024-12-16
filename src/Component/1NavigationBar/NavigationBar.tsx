cb6cb2311ed969b67577f581ae60876a9000eb1c
frehmanOnQC <fike.rehman@gmail.com>
2024-12-16 14:38:46 -0600

    setting the menus of the navigation bar

--- src/Component/1NavigationBar/NavigationBar.tsx
+++ src/Component/1NavigationBar/NavigationBar.tsx
@@ -9,11 +9,13 @@ const NavigationBar: React.FC<NavigationBarProps> = () => {
   return (
     <Navbar bg="dark" expand="lg" className="navbar">
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="nav-bar"> 
-        <Nav.Link href="#home" className="mx-auto nav-home">Home</Nav.Link>
-          <Nav.Link href="#about" className='nav-link'>About</Nav.Link>
-          <Nav.Link href="#projects" className='nav-link'>Projects</Nav.Link>
-          <Nav.Link href="#resume" className='nav-link'>Resume</Nav.Link>
+        <Nav className="nav-bar">
+          <Nav.Link href="#home" className='nav-home'>Home</Nav.Link>
+          <div className='nav-links'>
+            <Nav.Link href="#about" className='nav-link'>About</Nav.Link>
+            <Nav.Link href="#projects" className='nav-link'>Projects</Nav.Link>
+            <Nav.Link href="#resume" className='nav-link'>Resume</Nav.Link>
+          </div>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
