31e576e9a4ec24f5be8e419f3380183902f5fdc5
frehmanOnQC <fike.rehman@gmail.com>
2024-12-18 16:02:09 -0600

    adding content to the Home page

--- src/Components/1Home/Home.tsx
+++ src/Components/1Home/Home.tsx
@@ -1,10 +1,16 @@
-
+import foliopic from '../../assets/Images/folioPic.png';
+import './Home.css';
 
 const Home: React.FC = () => {
     return (
-        <div style={{ paddingTop: '100px', textAlign: 'center' }}>
-            <h1> This is the Home Page</h1>
-            <p> This is where you land </p>
+        <div className='home-container'>
+            <div className='home-content'>
+                <h1> This is the Home Page</h1>
+                <p> This is where I tell you about myself </p>
+            </div>
+            <div className='profile-container'>
+                <img src={foliopic} alt="Folio Picture" className='folio-pic' />
+            </div>
         </div>
     );
 };
