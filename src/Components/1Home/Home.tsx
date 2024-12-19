8c87cf37a7c013c9663aabacf97991841262705b
frehmanOnQC <fike.rehman@gmail.com>
2024-12-19 15:59:36 -0600

    Putting together the hoime page

--- src/Components/1Home/Home.tsx
+++ src/Components/1Home/Home.tsx
@@ -5,8 +5,10 @@ const Home: React.FC = () => {
     return (
         <div className='home-container'>
             <div className='home-content'>
-                <h1> This is the Home Page</h1>
-                <p> This is where I tell you about myself </p>
+                <h1 className="welcome-message">
+                    Hello There! <span className="waving-hand">👋</span>
+                </h1>
+                <h1> I am <span className='name'>Fike Rehman</span></h1>
             </div>
             <div className='profile-container'>
                 <img src={foliopic} alt="Folio Picture" className='folio-pic' />
