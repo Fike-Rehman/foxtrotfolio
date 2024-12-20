ab394dc54ced623cb7cbe46e6cb7ca80ba05faff
frehmanOnQC <fike.rehman@gmail.com>
2024-12-19 20:12:01 -0600

    Further enhancments to the Home page

--- src/Components/1Home/Home.tsx
+++ src/Components/1Home/Home.tsx
@@ -1,3 +1,4 @@
+import { Typewriter } from 'react-simple-typewriter';
 import foliopic from '../../assets/Images/folioPic.png';
 import './Home.css';
 
@@ -8,7 +9,23 @@ const Home: React.FC = () => {
                 <h1 className="welcome-message">
                     Hello There! <span className="waving-hand">👋</span>
                 </h1>
-                <h1> I am <span className='name'>Fike Rehman</span></h1>
+                <h1> I am <span className='name'>Fike Rehman;</span></h1>
+                <span className='typewriter-effect'><Typewriter
+                    words={['Software Engineer', 'Full Stack Developer', 'Tech Enthusiast', 'Adventure Seeker', 'Tinkerer/Maker', 'Wildlife Photographer']}
+                    loop={true}
+                    cursor
+                    cursorStyle="|"
+                    typeSpeed={70}
+                    deleteSpeed={50}
+                    delaySpeed={1000}
+                /></span>
+                <p className='welcome-message-secondary'>
+                    I am glad you stopped by, <br />
+                    Please feel free to look around or use the links <br />
+                    below to connect with me.<br />
+                    I would love to hear from you...
+                </p>
+
             </div>
             <div className='profile-container'>
                 <img src={foliopic} alt="Folio Picture" className='folio-pic' />
