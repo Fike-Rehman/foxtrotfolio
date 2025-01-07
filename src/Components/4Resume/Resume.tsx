f6d1c270f5d10bb1eae0b6b102f337fd32ed53e6
frehmanOnQC <fike.rehman@gmail.com>
2025-01-07 10:37:01 -0600

    updated Resume page with graphic in full screens

--- src/Components/4Resume/Resume.tsx
+++ src/Components/4Resume/Resume.tsx
@@ -5,13 +5,12 @@ const Resume: React.FC = () => {
     return (
         <div className="resume-container">
             <div className="resume-graphic">
-                <h1>Resume</h1>
+                <img src="/foxtrotfolio/images/MyJourneyHR.png" alt="My Journey" className='resume-image' />
             </div>
             <div className='resume-download'>
-                <div className='download-button-container'>
-                    <DownloadButton title="Download - Adobe PDF (.pdf)" fileUrl='/foxtrotfolio/docs/Resume2025.pdf' fileName='Resume2025.pdf'></DownloadButton>
-                    <DownloadButton title="Download - MS Word (.docx)" fileUrl='/foxtrotfolio/docs/Resume2025.docx' fileName='Resume2025.docx'></DownloadButton>
-                </div>
+                <DownloadButton title="Download - Adobe PDF (.pdf)" fileUrl='/foxtrotfolio/docs/Resume2025.pdf' fileName='Resume2025.pdf'></DownloadButton>
+                <DownloadButton title="Download - MS Word (.docx)" fileUrl='/foxtrotfolio/docs/Resume2025.docx' fileName='Resume2025.docx'></DownloadButton>
+
             </div>
         </div>
     );
