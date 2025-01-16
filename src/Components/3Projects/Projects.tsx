098c88ab5ef750d7112936caff8f0cc7fba7a886
frehmanOnQC <fike.rehman@gmail.com>
2025-01-16 12:40:36 -0600

    begining to introduce md files for project details

--- src/Components/3Projects/Projects.tsx
+++ src/Components/3Projects/Projects.tsx
@@ -1,14 +1,18 @@
 import React from "react";
-
+import projectDetails from "./ProjectDetails";
 import "react-multi-carousel/lib/styles.css";
 import "./Projects.css";
 import ProjectsCarousel from "../ProjectsCarousel/ProjectsCarousel";
 
+
 const Projects: React.FC = () => {
+
+    const pDetails = projectDetails;
+
     return (
         <div className="projects-page">
             <h1 className="projects-header">My Projects</h1>
-            <ProjectsCarousel />
+            <ProjectsCarousel projects={pDetails} />
             <footer className="projects-other-details">
                 <p>More information about my projects will be added here.</p>
             </footer>
