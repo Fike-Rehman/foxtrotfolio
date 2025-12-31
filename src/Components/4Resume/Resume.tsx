70dab77f705326b780ef9c50c89ec29eaa92398d
frehmanOnQC <fike.rehman@gmail.com>
2025-12-31 12:39:02 -0600

    uploaded new career journey graphic

--- src/Components/4Resume/Resume.tsx
+++ src/Components/4Resume/Resume.tsx
@@ -1,19 +1,30 @@
-import './Resume.css';
-import DownloadButton from '../DownloadButton/DownloadButton';
+import "./Resume.css";
+import DownloadButton from "../DownloadButton/DownloadButton";
 
 const Resume: React.FC = () => {
-    return (
-        <div className="resume-container">
-            <div className="resume-graphic">
-                <img src="/foxtrotfolio/images/MyJourneyHR.png" alt="My Journey" className='resume-image' />
-            </div>
-            <div className='resume-download'>
-                <DownloadButton title="Download - Adobe PDF (.pdf)" fileUrl='/foxtrotfolio/docs/Resume2026.pdf' fileName='Resume2026.pdf'></DownloadButton>
-                <DownloadButton title="Download - MS Word (.docx)" fileUrl='/foxtrotfolio/docs/Resume2026.docx' fileName='Resume2026.docx'></DownloadButton>
-
-            </div>
-        </div>
-    );
+  return (
+    <div className="resume-container">
+      <div className="resume-graphic">
+        <img
+          src="/foxtrotfolio/images/NB-CareerJourney.png"
+          alt="My Journey"
+          className="resume-image"
+        />
+      </div>
+      <div className="resume-download">
+        <DownloadButton
+          title="Download - Adobe PDF (.pdf)"
+          fileUrl="/foxtrotfolio/docs/Resume2026.pdf"
+          fileName="Resume2026.pdf"
+        ></DownloadButton>
+        <DownloadButton
+          title="Download - MS Word (.docx)"
+          fileUrl="/foxtrotfolio/docs/Resume2026.docx"
+          fileName="Resume2026.docx"
+        ></DownloadButton>
+      </div>
+    </div>
+  );
 };
 
 export default Resume;
